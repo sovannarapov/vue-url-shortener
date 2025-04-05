ded3f162e06267d1dbabaac65f0fdd4617b2c3ea
sovannarapov <sovannarapov9@gmail.com>
2025-04-05 22:47:50 +0700

    refactor: update interface of urlformdata

--- domain/interface.ts
+++ domain/interface.ts
@@ -1,8 +1,3 @@
-import type { HTTP_METHOD } from './constant';
-import type { ValueOf } from './type';
-
-export type HttpMethod = ValueOf<typeof HTTP_METHOD>;
-
 export interface UrlFormData {
   url: string;
 }
